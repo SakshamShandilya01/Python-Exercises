4779fc8c4d457dc535164561301fc6ca35973225
SakshamShandilya01 <sakshamshandilya91@gmail.com>
2026-05-26 03:19:52 +0530

    Initial Commit

--- calculator_basic.py
+++ calculator_basic.py
@@ -1,7 +1,20 @@
-num1 = float(input("Enter the  first number "))
-operation = input("Enter the operation you want to perform ")
-num2 = float(input("Enter the second number "))
+try:
+    num1 = float(input("Enter the  first number "))
+    operation = input("Enter the operation you want to perform ")
+    num2 = float(input("Enter the second number "))
 
-result = eval(f"{num1}{operation}{num2}") # eval() directly evaluates the mathematical expression as a string.
+    result = eval(f"{num1}{operation}{num2}")  # eval() directly evaluates the mathematical expression as a string.
 
-print("ANSWER: ", result)
+    print("ANSWER: ", result)
+
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
+
+except ValueError:
+    print("Please enter valid numbers.")
+
+except SyntaxError:
+    print("Invalid operation entered.")
+
+except Exception as e:
+    print("An error occurred:", e)
