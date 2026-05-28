9383c954fd15969c2cb71862cb6dba0cc570a848
SakshamShandilya01 <sakshamshandilya91@gmail.com>
2026-05-28 07:21:56 +0530

    Initial Commit

--- calculator_basic.py
+++ calculator_basic.py
@@ -1,3 +1,15 @@
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def divide(a,b):
+    return a/b
+
+def multiply(a,b):
+    return a*b
+
 try:
     num1 = float(input("Enter the  first number "))
     operation = input("Enter the operation you want to perform ")
@@ -18,3 +30,27 @@ except SyntaxError:
 
 except Exception as e:
     print("An error occurred:", e)
+
+def addition():
+    assert add(2,3) == 5
+    assert add(5,3) == 8
+    assert add (2,5) == 7
+    assert add(8,9) == 17
+
+def subtraction():
+    assert sub(5,3) == 2
+    assert sub(6, 5) == 1
+    assert sub(8, 9) == -1
+    assert sub(7, 4) == 3
+
+def divison():
+    assert divide(10,0) == "cannot divide by zero"
+    assert divide(8,4) == 2
+    assert divide(54,6) == 8
+    assert divide(124,4) == 31
+
+def multiplication():
+    assert multiply(8,7) == 56
+    assert multiply(5,9) == 45
+    assert multiply(9867,0) == 0
+    assert multiply(89,99) == 8811
