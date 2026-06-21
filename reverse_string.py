a5ca862deb7c34fbb6c423f656aab66e3384d46b
SakshamShandilya01 <sakshamshandilya91@gmail.com>
2026-06-21 22:57:56 +0530

    Initial Commit

---
+++ reverse_string.py
@@ -0,0 +1,12 @@
+arr = input("Enter characters without spaces: ")
+
+arr = list(arr)
+
+left, right = 0, len(arr) - 1
+
+while left < right:
+    arr[left], arr[right] = arr[right], arr[left]
+    left += 1
+    right -= 1
+
+print("Reversed array:", arr)
