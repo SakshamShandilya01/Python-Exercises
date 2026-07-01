01319b27e44ce54fc17e343252b888a3f71855fd
SakshamShandilya01 <sakshamshandilya91@gmail.com>
2026-07-01 21:28:30 +0530

    Initial Commit

---
+++ Sort_an_Array.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def sortArray(self, nums):
+        self.mergeSort(nums)
+        return nums
+
+    def mergeSort(self, arr):
+        if len(arr) > 1:
+            mid = len(arr) // 2
+
+            left = arr[:mid]
+            right = arr[mid:]
+
+            self.mergeSort(left)
+            self.mergeSort(right)
+
+            i = j = k = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+
+            while i < len(left):
+                arr[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                arr[k] = right[j]
+                j += 1
+                k += 1
