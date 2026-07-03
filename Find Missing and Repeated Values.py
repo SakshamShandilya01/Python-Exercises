7214033143fbd72f0b8410bd770b0842d0db9846
SakshamShandilya01 <sakshamshandilya91@gmail.com>
2026-07-03 16:59:12 +0530

    Initial Commit

---
+++ Find Missing and Repeated Values.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMissingAndRepeatedValues(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[int]
+        """
+
+        n=len(grid)
+        d=m=0
+        counts = {}
+        for i in range(n):
+            for j in range(n):
+                val = grid[i][j]
+                counts[val] = counts.get(val, 0) + 1
+        for num in range(1,n*n+1):
+            c = counts.get(num, 0)
+            if c==2:
+                d=num
+            elif c==0:
+                m=num
+        return [d,m]
