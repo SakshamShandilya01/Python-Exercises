54f078039deedb7f5781dc2b9d2b21e7bdba50cc
SakshamShandilya01 <sakshamshandilya91@gmail.com>
2026-07-11 22:51:39 +0530

    Initial Commit

---
+++ Koko Eating Bananas.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+
+        low = 1
+        high = max(piles)
+
+        while(low <= high):
+
+            mid = (low + high) // 2
+
+            hours = 0
+
+            for i in range(len(piles)):
+                hours += (piles[i] + mid - 1) // mid
+
+            if(hours <= h):
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
