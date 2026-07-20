823ec02c20444a184664c39a2a7e18eb8406b154
SakshamShandilya <sakshamshandilya91@gmail.com>
2026-07-20 14:56:40 +0530

    Time: 1 ms (57.25%), Space: 12.3 MB (93.52%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -4,5 +4,15 @@ class Solution(object):
         :type sentences: List[str]
         :rtype: int
         """
-        
+        max = 0
+
+        for i in sentences:
+
+            words = i.split()
+            count = len(words)
+
+            if count>max:
+                max = count
+        return max
+
         
